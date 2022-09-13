83c14e78efbb316c1bb3d7543bf992286fb42db3
Katherine Asenjo <>
2022-09-13 15:13:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+
+nombre = input("Ingrese su nombre :")
+print("===========================")
+print("Bienvenido(a) : " + nombre)
+print("Esta es mi primera trivia, \nveamos como nos va")
+print("===========================")
+print("Pregunta 1:¿Quién es el creador de Python? ")
+print("Alternativas:")
+print("a) Guido van Rossum")
+print("b) James Gosling")
+print("c) Larry Page")
+print("d) Sundar Pichai")
+print("===========================")
+respuestaPregunta_1 = input("por favor escribir la alternativa: a, b, c o d : ")
+if respuestaPregunta_1 == "a":
+  print("Alternativa correcta: a")
+else:
+  print("Alternativa incorrecta: " + respuestaPregunta_1)
+print("===========================")
+print("Pregunta 2:¿Qué es el BACKEND? ")
+print("Alternativas:")
+print("a) Es la parte del desarrollo web que se dedica a la parte frontal de un sitio web")
+print("b) Es un área enfocada en el desarrollo de interfaces digitales")
+print("c) Es un sistema corporativo que se utilizan para dirigir una web o empresa")
+print("d) Es un lenguaje que maneja el diseño y presentación de las páginas web")
+print("===========================")
+respuestaPregunta_2 = input("por favor escribir la alternativa: a, b, c o d : ")
+if respuestaPregunta_2 == "c":
+  print("Alternativa correcta: c")
+else:
+  print("Alternativa incorrecta: " + respuestaPregunta_2)
+print("===========================")
+print("Pregunta 3:¿Cuál de las siguientes altenativas son navegadores? ")
+print("Alternativas:")
+print("a) Java, C++.")
+print("b) Mozilla Firefox, Microsoft Edge ")
+print("c) PHP,Objective-C")
+print("d) Swift,R")
+print("===========================")
+respuestaPregunta_3 = input("por favor escribir la alternativa: a, b, c o d : ")
+if respuestaPregunta_3 == "b":
+  print("Alternativa correcta: b")
+else:
+  print("Alternativa incorrecta: " + respuestaPregunta_3)
+print("===========================")
+
+
